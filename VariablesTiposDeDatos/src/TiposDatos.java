d0d591ebfa092f6c93c5874578b15db3fff89da6
Carlos Eduardo <c.eduardo.jesus@outlook.com>
2021-06-30 23:16:49 -0500

    Primer commit Java

---
+++ VariablesTiposDeDatos/src/TiposDatos.java
@@ -0,0 +1,30 @@
+
+public class TiposDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println("Hola Mundo");
+		
+		int numero1 = 30;
+		int numero2 = 6;
+		int mutiplicacion = numero1 * numero2;
+		String cadena1 = "texto 1";
+		String cadena2 = "texto 2";
+		char letra = 'y';
+		
+		String oracion = cadena1 + cadena2;
+		
+		boolean mayor18 = true;
+		
+		System.out.println(oracion);
+		
+		
+		
+		
+		
+
+	}
+
+}
